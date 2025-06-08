9ff6af4e731218379f5fe00d7499f331c16f4b5c
nohsueh <nohsueh@qq.com>
2025-06-08 19:27:28 +0800

    a

---
+++ main.py
@@ -0,0 +1,41 @@
+from raven_gen import Matrix, MatrixType
+from datetime import datetime
+import os
+from time import sleep
+
+matrix_types = [
+    MatrixType.ONE_SHAPE,
+    MatrixType.ONE_SHAPE,
+    MatrixType.TWO_SHAPE_VERTICAL_SEP,
+    MatrixType.TWO_SHAPE_VERTICAL_SEP,
+    MatrixType.TWO_SHAPE_HORIZONTAL_SEP,
+    MatrixType.TWO_SHAPE_HORIZONTAL_SEP,
+    MatrixType.SHAPE_IN_SHAPE,
+    MatrixType.SHAPE_IN_SHAPE,
+    MatrixType.SHAPE_IN_SHAPE,
+    MatrixType.SHAPE_IN_SHAPE,
+    MatrixType.FOUR_SHAPE,
+    MatrixType.FOUR_SHAPE,
+    MatrixType.FOUR_SHAPE,
+    MatrixType.FOUR_SHAPE,
+    MatrixType.FOUR_SHAPE_IN_SHAPE,
+    MatrixType.FOUR_SHAPE_IN_SHAPE,
+    MatrixType.FOUR_SHAPE_IN_SHAPE,
+    MatrixType.FOUR_SHAPE_IN_SHAPE,
+    MatrixType.FIVE_SHAPE,
+    MatrixType.FIVE_SHAPE,
+    MatrixType.FIVE_SHAPE,
+    MatrixType.FIVE_SHAPE,
+    MatrixType.NINE_SHAPE,
+    MatrixType.NINE_SHAPE,
+    MatrixType.NINE_SHAPE,
+]
+
+date = datetime.now().strftime("%Y%m%d_%H%M%S")
+for index, matrix_type in enumerate(matrix_types):
+    rpm = Matrix.make(matrix_type=matrix_type, n_alternatives=6)
+    os.makedirs(f"./output/{index}_{matrix_type.name}/{date}", exist_ok=True)
+    puzzle_name = "rpm"
+    rpm.save(path=f"./output/{index}_{matrix_type.name}/{date}", puzzle_name=puzzle_name, image_size=1024)
+
+    sleep(1)
